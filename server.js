bb4112ccdda233af7dfc7e0f63f58e2ec8b8d654
Ankur21bera <159689731+Ankur21bera@users.noreply.github.com>
2026-02-10 16:42:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDb from './config/db.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDb from "./config/db.js";
+
 import { serve } from "inngest/express";
-import { inngest,functions } from './inngest/index.js';
+import { inngest, functions } from "./inngest/index.js";
 
-
-import {clerkMiddleware} from '@clerk/express'
-import userRouter from './Route/userRoute.js';
-
-
-
+import { clerkMiddleware } from "@clerk/express";
+import userRouter from "./Route/userRoute.js";
 
 const app = express();
 
-connectDb()
+// Connect DB
+await connectDb();
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-app.get("/",(req,res)=>res.send("Server Is Running"))
-app.use("/api/inngest", serve({ client: inngest, functions }));
+// Test route
+app.get("/", (req, res) => res.send("Server Is Running"));
 
+// Clerk middleware (for your normal APIs)
 app.use(clerkMiddleware());
 
-app.get("/",(req,res)=>res.send("Server Is Running"))
+// Inngest endpoint (webhooks)
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/user",userRouter)
 
+// API routes
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT,()=>console.log(`Server Is Running On Port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
